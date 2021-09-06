4d1309ab22bf226e0eb4b29e87077b195abc3301
Charley Wright <charleywright06@gmail.com>
2021-09-06 20:53:35 +0100

    Cleaned up sigs

--- Code/Game.cpp
+++ Code/Game.cpp
@@ -11,13 +11,10 @@ namespace Cheese
 									 m_GameWindow(FindWindowA("grcWindow", "Grand Theft Auto V")),
 									 m_Swapchain(Signature("48 8B 0D ? ? ? ? 48 8D 55 A0 48 8B 01").Scan().Add(3).Rip().As<decltype(m_Swapchain) &>()),
 									 m_NativeRegistrations(Signature("48 83 EC 20 48 8D 0D ? ? ? ? E8 ? ? ? ? 0F B7 15 ? ? ? ? 33 FF").Scan().Add(7).Rip().As<decltype(m_NativeRegistrations)>()),
-									 m_GlobalBase(Signature("4C 8D 4D 08 48 8D 15 ? ? ? ? 4C 8B C0").Scan().Add(7).Rip().As<decltype(m_GlobalBase)>())
+									 m_GlobalBase(Signature("4C 8D 4D 08 48 8D 15 ? ? ? ? 4C 8B C0").Scan().Add(7).Rip().As<decltype(m_GlobalBase)>()),
+									 m_WorldPtr(Signature("48 8B 05 ? ? ? ? 45 ? ? ? ? 48 8B 48 08 48 85 C9 74 07").Scan().Add(3).Rip().As<decltype(m_WorldPtr)>()),
+									 m_GameBuild(Signature("48 83 EC 60 48 8D 0D ? ? ? ? E8").Scan().Sub(17).Add(265 + 3).Rip().As<decltype(m_GameBuild)>())
 	{
-		MemoryHandle gameBuildSig = Signature("48 83 EC 60 48 8D 0D ? ? ? ? E8").Scan().Sub(17);
-		m_GameBuild = gameBuildSig.Add(265 + 3).Rip().As<decltype(m_GameBuild)>();
-
-		MemoryHandle worldPtrSig = Signature("48 8B 05 ? ? ? ? 45 ? ? ? ? 48 8B 48 08 48 85 C9 74 07").Scan();
-		m_WorldPtr = worldPtrSig.Add(*worldPtrSig.Add(3).As<int *>()).Add(7).As<decltype(m_WorldPtr)>();
 	}
 
 	void GameVariables::PostInit()
